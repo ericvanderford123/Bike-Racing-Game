08f4ba9f4d5c96eaebfee5797396409c5bdb4fde
evanderford.f19 <evanderford.f19@warren-wilson.edu>
2021-11-29 15:47:00 -0500

    class 11/29/21

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import pygame
 
 pygame.init()
 
-w,h =1500,750
+w,h =1200,750
 
 window=pygame.display.set_mode((w,h))
+pygame.display.set_caption("Bicycle Racing Game")
+icon= pygame.image.load("data/bicycle racing.png")
+pygame.display.set_icon(icon)
 
+background=pygame.image.load("data/bike backdrop.png")
 start=True
 
 while start:
+    window.blit(background, (0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            start=False
+            start = False
             pygame.quit()
             quit()
+    pygame.display.update()
+    

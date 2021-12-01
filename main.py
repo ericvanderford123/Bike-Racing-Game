04217a5d24fe5363566e3bc841ef9de13d24f9e7
evanderford.f19 <evanderford.f19@warren-wilson.edu>
2021-12-01 15:10:57 -0500

    class 11/29/21

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ window=pygame.display.set_mode((w,h))
 pygame.display.set_caption("Bicycle Racing Game")
 icon= pygame.image.load("data/bicycle racing.png")
 pygame.display.set_icon(icon)
-
-background=pygame.image.load("data/bike backdrop.png")
+background=pygame.image.load('data/bike backdrop.png')
+background = pygame.transform.scale(background, (1200, 750))
 start=True
 
 while start:

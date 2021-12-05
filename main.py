c0698701c45aa370d7e02790a7e05aaef902d49b
ericvanderford <ericvanderford123@gmail.com>
2021-12-05 18:35:03 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -17,15 +18,17 @@ bike2=pygame.image.load('data/bike1.png')
 bike2 = pygame.transform.scale(bike2, (200, 150))
 finishingline=pygame.image.load('data/finishline.png')
 finishingline = pygame.transform.rotate(finishingline, 90)
-finishingline = pygame.transform.scale(finishingline, (600, 1000))
+finishingline = pygame.transform.scale(finishingline, (500, 900))
 
 #position of the first bike
 b1x=0
 b1y=100
+b1change=0
 
 #position of the second bike
 b2x=0
 b2y=400
+b2change=0
 x,y=900,-50
 def player(x,y):
     window.blit(bike1,(x,y))
@@ -43,7 +46,12 @@ while start:
             pygame.quit()
             quit()
 
-
+    move1=random.randint(1,4)
+    move2 = random.randint(1, 4)
+    b1change=move1
+    b2change = move2
+    b1x= b1x+ b1change
+    b2x = b2x + b2change
     player(b1x,b1y)
     player(b2x,b2y)
     pygame.display.update()
